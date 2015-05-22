d35d3c7c3e04f85e817b992d96b61ef7ca16956c
NathanGloyn <nathans.dropbox@gmail.com>
2015-05-22 17:25:49 +0100

    Add WebApi test harness

---
+++ Src/Loupe.Agent.Web.Module.WebApiTest/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Mvc;
+
+namespace Loupe.Agent.Web.Module.MVCTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.SessionId = ControllerContext.HttpContext.Items["LoupeSessionId"].ToString();
+
+            return View();
+        }
+    }
+}
