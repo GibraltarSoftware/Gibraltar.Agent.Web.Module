c2261478e028e96bbcb1dea9bdeee6a1c8637878
NathanGloyn <nathans.dropbox@gmail.com>
2015-05-27 11:15:04 +0100

    Add File Header block to all files in module project

--- Src/Loupe.Agent.Web.Module/DetailBuilders/DetailsBuilderBase.cs
+++ Src/Loupe.Agent.Web.Module/DetailBuilders/DetailsBuilderBase.cs
@@ -1,9 +1,29 @@
-﻿using System.IO;
+﻿#region File Header
+
+// <copyright file="DetailsBuilderBase.cs" company="Gibraltar Software Inc.">
+// Gibraltar Software Inc. All rights reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System.IO;
 using System.Text;
 using System.Xml;
 using System.Xml.Serialization;
 using Gibraltar.Agent;
 using Newtonsoft.Json;
+
+#endregion
 
 namespace Loupe.Agent.Web.Module.DetailBuilders
 {
